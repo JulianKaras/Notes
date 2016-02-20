ccdfc2d14a84d6bcc1c9879fcc6c85c435fd762f
JulianKaras <julian101@hotmail.com>
2016-02-19 19:44:41 -0500

    Recycler view

--- app/src/main/java/com/barkerville/notes/MainActivity.java
+++ app/src/main/java/com/barkerville/notes/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
@@ -13,12 +15,22 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+        private RecyclerView mRecyclerView;
+        private LinearLayoutManager mLayoutManager;
+        private NoteListItemAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new NoteListItemAdapter(this, mRecyclerView);
+        mRecyclerView.setAdapter(mAdapter);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
---
+++ app/src/main/java/com/barkerville/notes/NoteListItem.java
@@ -0,0 +1,17 @@
+package com.barkerville.notes;
+
+/**
+ * Created by JulianK on 19/02/2016.
+ */
+public class NoteListItem {
+
+    private String text;
+
+    public NoteListItem (String text){
+        this.text = text;
+    }
+
+    public String getText (){
+        return this.text;
+    }
+}
