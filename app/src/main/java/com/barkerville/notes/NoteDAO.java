648c46dddd62a32cbaa377684e56598fb40eb73b
JulianKaras <julian101@hotmail.com>
2016-03-05 23:46:00 -0500

    querying the database

---
+++ app/src/main/java/com/barkerville/notes/NoteDAO.java
@@ -0,0 +1,71 @@
+package com.barkerville.notes;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabaseCorruptException;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+/**
+ * Created by JulianK on 05/03/2016.
+ */
+public class NoteDAO {
+
+    private Context context;
+
+    public NoteDAO(Context context) {
+
+        this.context = context;
+    }
+
+    public void save(NoteListItem note) {
+
+        NotesDBHelper helper = new NotesDBHelper.getInstance(context);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(NotesDBContract.Note.COLUMN_NAME_NOTE_TEXT, note.getText());
+        values.put(NotesDBContract.Note.COLUMN_NAME_STATUS, note.getStatus());
+        values.put(NotesDBContract.Note.COLUMN_NAME_NOTE_DATE, (note.getDate().getTimeInMillis()/1000));
+
+        db.insert(NotesDBContract.Note.TABLE_NAME, null, values);
+    }
+
+    public List<NoteListItem> list() {
+        NotesDBHelper helper = new NotesDBHelper.getInstance(context);
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String[] projection = {
+
+                NotesDBContract.Note.COLUMN_NAME_NOTE_TEXT,
+                NotesDBContract.Note.COLUMN_NAME_STATUS,
+                NotesDBContract.Note.COLUMN_NAME_NOTE_DATE
+        };
+
+        String sortOrder = NotesDBContract.Note.COLUMN_NAME_NOTE_DATE + "DESC";
+
+        Cursor c = db.query(
+                NotesDBContract.Note.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                sortOrder
+        );
+
+        while (c.moveToNext()) {
+            String text = c.getString(c.getColumnIndex(NotesDBContract.Note.COLUMN_NAME_NOTE_TEXT));
+            String status = c.getString(c.getColumnIndex(NotesDBContract.Note.COLUMN_NAME_STATUS));
+            Calendar date = new GregorianCalendar();
+            date.setTimeInMillis(c.getLong(c.getColumnIndex(NotesDBContract.Note.COLUMN_NAME_NOTE_DATE))*1000);
+            notes.add(new NoteListItem(text, status, date));
+        }
+            return notes;
+
+    }
+}
--- app/src/main/java/com/barkerville/notes/NoteListItem.java
+++ app/src/main/java/com/barkerville/notes/NoteListItem.java
@@ -1,17 +1,57 @@
 package com.barkerville.notes;
 
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.Calendar;
+
 /**
  * Created by JulianK on 19/02/2016.
  */
-public class NoteListItem {
+public class NoteListItem implements Serializable{
 
+    private long id;
     private String text;
+    private String status;
+    private Calendar date;
 
     public NoteListItem (String text){
+
         this.text = text;
+
     }
+
+   //*** public NoteListItem(String text, String status, Calendar date) {
+   //*** }
 
     public String getText (){
         return this.text;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
     }
 }
